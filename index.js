0bd83ade88877ebc6e6b02722e346dc6c92be362
ddderi <maxime830@live.fr>
2022-01-18 04:41:18 +0100

    commit tree 4db82319ec13156e9f7d880e1669759a78e9d5b7

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+const add = input.addEventListener('click', function(e){
+    
+})
 }
+
+
+
+// const input = document.getElementById('input');
+// const add = input.addEventListener('click', function(e){
+
+// })
